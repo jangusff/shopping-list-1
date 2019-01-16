c5e9057b413d4b41b576ab47722692b4f3edf693
Jonathan Angus <jangusff@gmail.com>
2019-01-16 16:26:35 -0600

    attempts to get add-to-list functionality to work

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
 $(function() {
 
+
+  const li_template = $("ul.shopping-list").first();
+console.log(li_template);
+
+  //$(".shopping-list").find("li").remove();
+  //console.log(li_template);
+  //console.log($(".shopping-list"));
+
+$("ul.shopping-list").append($(li_template));
+
+  $("#js-shopping-list-form").submit(function(event) {
+    console.log("here3");
+    event.preventDefault();
+    const newItemToAdd = $(this).find('input[name="shopping-list-entry"]');
+    
+    let liToAdd = li_template;
+    
+    $(liToAdd).find(".shopping-item").text(newItemToAdd.val());
+    console.log(liToAdd);
+
+    $("ul.shopping-list").append($(liToAdd));
+    newItemToAdd.val("");    
+    // <span class="shopping-item">apples</span>
+
+
+  });
+
+/*
+  $("button").click(function(event) {
+    $("ul").append(
+      "<li>" +
+      ["cat", "dog", "rock"][Math.floor(Math.random()*3)] + "</li>"
+    );
+  });
+*/
   $("button.shopping-item-toggle").on("click", function( event ) {
       $(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
   });
